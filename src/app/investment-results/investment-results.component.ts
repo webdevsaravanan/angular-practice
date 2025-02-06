630b0571060ad89abc3a4d0421da07ec75fbc947
webdevsaravanan <webdevsaravanan@gmail.com>
2025-02-06 23:30:33 +0530

    Currency Pipe

--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import type { InvestmentResult } from '../invesment-input.model';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-investment-results',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.css'
 })
